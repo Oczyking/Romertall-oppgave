91bf620766f622c34d9210bd097d64c6c1c4e67a
Oczy_King <mat-loe@outlook.com>
2021-08-26 17:59:36 +0200

    Initial commit

---
+++ test/ShowRomanLetters.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShowRomanLetters {
+
+    @Test
+    void shouldConvert1ToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int i) {
+        return "I";
+    }
+}
